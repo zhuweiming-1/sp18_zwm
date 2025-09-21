88aec09852fea5493703f93f71bf8b768ba0925c
zhuweiming-1 <15123001642@163.com>
2025-09-21 16:37:47 +0800

    lab15 mod2

--- lab15/creatures/TestClorus.java
+++ lab15/creatures/TestClorus.java
@@ -1,5 +1,25 @@
 package creatures;
 
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestClorus {
 
+    @Test
+    public void testBasics() {
+        Clorus p = new Clorus(2);
+        assertEquals(2, p.energy(), 0.01);
+        assertEquals(new Color(34, 0, 231), p.color());
+        p.move();
+        assertEquals(1.97, p.energy(), 0.01);
+        p.move();
+        assertEquals(1.94, p.energy(), 0.01);
+        p.stay();
+        assertEquals(1.93, p.energy(), 0.01);
+        p.stay();
+        assertEquals(1.92, p.energy(), 0.01);
+    }
 }
